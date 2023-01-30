0169307477633d8805fc533af95c331ca352ad1a
vic33tor <vic33tor.vic@gmail.com>
2023-01-30 13:39:19 +0100

    Ej6 y Ej7

--- MySqli/edita_usuario_001.php
+++ MySqli/edita_usuario_001.php
@@ -15,28 +15,35 @@ echo "Conexión correcta"."<br>";
 
 $nombre = $_POST['nombre'];
 $mail = $_POST['mail'];
+$nombreNuevo = $_POST['nombreNuevo'];
+$mailNuevo = $_POST['mailNuevo'];
 
-if ($nombre=="" && $mail=="") {
+if($nombre == ""){
+    $sql = "UPDATE usuario SET nombre = '$nombreNuevo', mail = '$mailNuevo'
+    WHERE mail = '$mail'";
+    $result = $conn->query($sql);
+}elseif($mail == ""){
+    $sql = "UPDATE usuario SET nombre = '$nombreNuevo', mail = '$mailNuevo'
+    WHERE nombre = '$nombre'";
+    $result = $conn->query($sql);
+}else{
+    $sql = "UPDATE usuario SET nombre = '$nombreNuevo', mail = '$mailNuevo'
+    WHERE nombre = '$nombre' AND mail = '$mail'";
+    $result = $conn->query($sql);
+}
+
     $sql = "SELECT * FROM usuario";
     $result = $conn->query($sql);
-    // mostramos los resultados por filas
-    while($row = $result->fetch_assoc()) {
-        echo "<br>" . "id: " . $row["ID"]. " - nombre: " . $row["nombre"]. " mail: " .
-        $row["mail"]."<br>";
-    }
-} else {
-    $sql = "SELECT * FROM usuario WHERE nombre='$nombre'AND mail='$mail'";
-    $result = $conn->query($sql);
+
     if ($result->num_rows > 0) {
         // mostramos los resultados por filas
         while($row = $result->fetch_assoc()) {
             echo "<br>" . "id: " . $row["ID"]. " - nombre: " . $row["nombre"]. " mail: " .
-        $row["mail"]."<br>";
+            $row["mail"]."<br>";
         }
-    } else {
-        echo "0 results";
     }
-}
+
+
 
 $conn->close();
 ?>
